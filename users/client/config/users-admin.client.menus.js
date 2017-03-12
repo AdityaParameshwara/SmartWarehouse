df49f617ec49c2c41b227f2903f002b39fff7789
AdityaParameshwara <adityapbelli.1992@gmail.com>
2017-03-12 15:41:09 -0700

    Add files via upload

---
+++ users/client/config/users-admin.client.menus.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('users.admin')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['Menus'];
+
+  // Configuring the Users module
+  function menuConfig(Menus) {
+    Menus.addSubMenuItem('topbar', 'admin', {
+      title: 'Manage Users',
+      state: 'admin.users'
+    });
+  }
+}());
